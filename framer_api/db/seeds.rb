8303cccb3de945fc92f7b29bd5dafa06544d4d5b
Paul Farino <paul.farino1@gmail.com>
2017-03-01 15:18:39 -0800

    Seed DB with 1 record

--- framer_api/db/seeds.rb
+++ framer_api/db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(id: 1, name: "Paul Farino", title: "Product Designer", created_at: "2017-03-01 22:51:25", updated_at: "2017-03-01 22:51:25")
